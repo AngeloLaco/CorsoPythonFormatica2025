1b0923269428eff7cfb1e54fef7b24be2aa0a151
fabiomascia80-spec <fabiomascia80@gmail.com>
2025-12-23 23:43:16 +0100

    Rinominato prova.py presentazione.py

--- esercizi_fabio/txt_casa/prova.py
+++
@@ -1,6 +0,0 @@
-nome= input("Inserisci il tuo nome: ")
-età= 34
-sesso= input("Inserisci il tuo sesso (M/F): ")
-altezza= 1.95
-nazionalità= "Italiana"
-print("Ciao\n mi chiamo {nome}, ho {età} anni, sono di sesso {sesso}, la mia altezza è {altezza} metri e la mia nazionalità è {nazionalità}.".format(nome=nome, età=età, sesso=sesso, altezza=altezza, nazionalità=nazionalità))
