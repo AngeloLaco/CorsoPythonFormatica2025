a2bcad3a22a748557168a60eb6b890144752d8a2
fabiomascia80-spec <fabiomascia80@gmail.com>
2025-12-24 17:30:00 +0100

    calcolatore

---
+++ esercizi_fabio/txt_casa/calcoli.py
@@ -0,0 +1,7 @@
+a = 5
+b = 7
+c = 4
+print(f'addizione: {a} + {b} +{c} = {a+b+c}')
+print(f'sottrazione: {a} - {b} - {c} = {a-b-c}')
+print(f'moltiplicazione: {a} * {b} * {c} = {a*b*c}')
+print(f'divisione: {a} / {b} / {c} = {a/b/c}')
