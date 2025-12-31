ed29448e65eea5260aea8bd23587e7b226321fdf
fabiomascia80-spec <fabiomascia80@gmail.com>
2025-12-31 02:23:04 +0100

    Calcolo dell'età

---
+++ esercizi_fabio/txt_casa/calcolo_età.py
@@ -0,0 +1,9 @@
+anno_corrente = 2024
+anno_nascita = int(input("In che anno sei nato/a? "))
+mese_nascita = input("In che mese sei nato/a? ")
+compiuti = input("Li hai già compiuti quest anno? (si/no) ").lower()
+eta = anno_corrente - anno_nascita
+if compiuti == "no":
+    eta -= 1
+print(f"Ricapitoliamo! Sei nato/a a {mese_nascita} del {anno_nascita}, oggi hai {eta} anni")
+1
