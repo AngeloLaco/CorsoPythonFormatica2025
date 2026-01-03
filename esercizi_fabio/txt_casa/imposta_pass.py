5c6878ff4e2ae0136f65ce919da76befa2413d40
fabiomascia80-spec <fabiomascia80@gmail.com>
2026-01-03 18:59:28 +0100

    Modifica programma pass

--- esercizi_fabio/txt_casa/imposta_pass.py
+++ esercizi_fabio/txt_casa/imposta_pass.py
@@ -1,14 +1,17 @@
- input("Inserisci una nuova password: ")
-lunghezza_ok = len(password) >= 8
-contiene_numero = any(chair.isdigit() for char in password)
-non_banale = password not in ["password", "12345678"]
-if lunghezza_ok and contiene_numero and non_banale:
+while True:
+ password = input("Inserisci una nuova password: ")
+ lunghezza_ok = len(password) >= 8
+ contiene_numero = any(char.isdigit() for char in password)
+ non_banale = password not in ["password", "12345678"]
+ if lunghezza_ok and contiene_numero and non_banale:
    print("Password sicura! Registrazione completata!")
-else:
+   break
+ else:
     print("Password NON sicura. Controlla i criteri:")
     if not lunghezza_ok:
-        print("Deve contenere almeno 8 caratteri:")
+     print("Deve contenere almeno 8 caratteri:")
     if not contiene_numero:
-        print("Deve contenere almeno un numero:")
+     print("Deve contenere almeno un numero:")
     if not non_banale:
-        print("Non puo essere una password troppo comune:")
+     print("Non puo essere una password troppo comune:")
+    print("Riprova..\n")
